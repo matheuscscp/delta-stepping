87b26304af47a19b993fa10f346d96dd45816717
Matheus Pimenta <matheuscscp@gmail.com>
2014-11-19 19:37:26 -0200

    Creating a basic test

--- src/test.cpp
+++ src/test.cpp
@@ -16,42 +16,41 @@
 using namespace std;
 using namespace graph;
 
-static int max_order = 20;
-static int max_weight = 50;
-static int graphs = 1000;
+static int n_threads = 1;
+static int delta = 1;
+static int max_weight = 1000;
+static int order = 1000;
 
-static inline bool generateAndCompare() {
+static inline void generateAndRun() {
   IntArrayGraphMapNeighbourHood G;
   
   // [2, max_order] vertices, weights in [1, max_weight]
-  generateGraph(G, rand()%(max_order - 1) + 2, max_weight);
+  generateGraph(G, order, max_weight);
   
-  IntAllPairsShortestPaths res1(G, IntDijkstra());
-  IntAllPairsShortestPaths res2(G, IntSerialDeltaStepping());
   IntAllPairsShortestPaths res3(G, IntParallelDeltaStepping());
-  
-  return (res1 == res2) && (res2 == res3);
 }
 
 void test(int argc, char** argv) {
   srand(time(nullptr));
-  int delta = 1;
   if (argc >= 2) {
-    sscanf(argv[1], "%d", &delta);
+    sscanf(argv[1], "%d", &n_threads);
+  }
+  if (argc >= 3) {
+    sscanf(argv[2], "%d", &delta);
+  }
+  if (argc >= 4) {
+    sscanf(argv[3], "%d", &max_weight);
+  }
+  if (argc >= 5) {
+    sscanf(argv[4], "%d", &order);
   }
   DeltaStepping::delta(&delta);
   DeltaStepping::initRelaxations();
-  int n_threads = 1;
-  if (argc >= 3) {
-    sscanf(argv[2], "%d", &n_threads);
-  }
   ThreadManager::init(n_threads);
   
-  int i = 0;
-  for (; i < graphs && generateAndCompare(); i++);
-  if (i < graphs) {
-    printf("erro! algum algoritmo nao esta correto\n");
-  }
+  generateAndRun();
+  
+  printf("relaxations: %lu\n", DeltaStepping::relaxations());
   
   ThreadManager::close();
   DeltaStepping::closeRelaxations();
