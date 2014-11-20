746e44ec4f0dbfc0260f8fb4829a8b93a97c2fdd
Matheus Pimenta <matheuscscp@gmail.com>
2014-11-20 12:54:03 -0200

    Removing some cmd args from test

--- src/test.cpp
+++ src/test.cpp
@@ -17,11 +17,6 @@
 using namespace std;
 using namespace graph;
 
-static int n_threads = 1;
-static int delta = 1;
-static int max_weight = 100;
-static int order = 100;
-
 static inline float readAndRun() {
   IntArrayGraphMapNeighbourHood G;
   scanDirectedGraph(G, stdin);
@@ -38,17 +33,13 @@ static inline float readAndRun() {
 }
 
 void test(int argc, char** argv) {
+  int delta = 1;
   if (argc >= 2) {
-    sscanf(argv[1], "%d", &n_threads);
+    sscanf(argv[1], "%d", &delta);
   }
+  int n_threads = 1;
   if (argc >= 3) {
-    sscanf(argv[2], "%d", &delta);
-  }
-  if (argc >= 4) {
-    sscanf(argv[3], "%d", &max_weight);
-  }
-  if (argc >= 5) {
-    sscanf(argv[4], "%d", &order);
+    sscanf(argv[2], "%d", &n_threads);
   }
   
   srand(time(nullptr));
